c62699f5138256abd3d665495c19b12b8925dbf3
Hector Figueroa <hecthorne@hotmail.com>
2015-02-11 13:45:41 -0300

    Atualizacao

---
+++ Acesso/Conexao.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoBD.Acesso
+{
+
+    public class Conexao
+    {
+        public DbProviderFactory FabricaAcesso { get; private set; }
+        public DbConnection _con { get; private set; }
+        public DbTransaction _trans { get; private set; }
+        public bool _comTrans { get; private set; }
+
+        private String sufixoConexao = "";
+
+        #region Tratar Conexao
+
+        public Conexao()
+        {
+            IniciaClasse(false);
+        }
+
+        public Conexao(String sufixoConexao)
+        {
+            this.sufixoConexao = sufixoConexao;
+            IniciaClasse(false);
+        }
+
+        public Conexao(bool iniciaTransacao)
+        {
+            IniciaClasse(iniciaTransacao);
+        }
+
+        public Conexao(bool iniciaTransacao, String sufixoConexao)
+        {
+            this.sufixoConexao = sufixoConexao;
+            IniciaClasse(iniciaTransacao);
+        }
+
+        private void IniciaClasse(bool iniciaTransacao)
+        {
+            FabricaAcesso = DbProviderFactories.GetFactory(ConfigurationManager.AppSettings["Provedor" + sufixoConexao]);
+            _con = FabricaAcesso.CreateConnection();
+            _con.ConnectionString = ConfigurationManager.AppSettings["ConnectionString" + sufixoConexao];
+            _comTrans = iniciaTransacao;
+
+            if (_comTrans)
+            {
+                _con.Open();
+                _trans = _con.BeginTransaction();
+            }
+        }
+        #endregion
+
+        #region Abrir e Fechar Conexão
+        public void AbreConexao()
+        {
+            _con.Open();
+        }
+
+        public void FechaConexao()
+        {
+            _con.Close();
+        }
+
+        public bool ConexaoFechada()
+        {
+            return _con.State == ConnectionState.Closed;
+        }
+        #endregion
+
+        #region Comando de Transacao
+
+        public void IniciarTransacao()
+        {
+            if (_comTrans)
+            {
+                throw new Exception("Já existe uma transação em aberto.");
+            }
+            if (ConexaoFechada())
+                AbreConexao();
+
+            _comTrans = true;
+            _trans = _con.BeginTransaction();
+        }
+
+        public void ConcluirTransacao()
+        {
+            if (!_comTrans)
+            {
+                throw new Exception("Não existe nenhuma transação em aberto.");
+            }
+            _trans.Commit();
+            _con.Close();
+            _comTrans = false;
+        }
+
+        public void CancelaTransacao()
+        {
+            if (!_comTrans)
+            {
+                throw new Exception("Não existe nenhuma transação em aberto.");
+            }
+            _trans.Rollback();
+            _con.Close();
+            _comTrans = false;
+        }
+        #endregion
+    }
+}
---
+++ Acesso/TratamentoRetorno.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ConexaoBD.Acesso
+{
+    public class TratamentoRetorno
+    {
+        #region Propriedades
+
+        private DbProviderFactory _fabricaAcesso;
+        private DbConnection _con;
+        private DbTransaction _trans;
+        private bool _comTrasacao;
+
+        #endregion
+
+        #region Contrutor
+
+        public TratamentoRetorno(DbProviderFactory fabricaAcesso, DbConnection con, DbTransaction trans, bool comTrasacao)
+        {
+            _fabricaAcesso = fabricaAcesso;
+            _con = con;
+            _trans = trans;
+            _comTrasacao = comTrasacao;
+        }
+
+        #endregion
+
+        #region Com Dapper
+        public List<T> ObterRetorno<T>(string instSQL, params DbParameter[] parametros)
+        {
+            DbCommand command = _fabricaAcesso.CreateCommand();
+            command.Connection = _con;
+            command.CommandTimeout = int.MaxValue;
+
+            return command.Connection.Query<T>(instSQL, parametros, _trans).ToList();
+        }
+
+        public List<T> ObterRetorno<T>(string instSQL)
+        {
+            DbCommand command = _fabricaAcesso.CreateCommand();
+            command.Connection = _con;
+            command.CommandTimeout = int.MaxValue;
+
+            return command.Connection.Query<T>(instSQL, transaction: _trans).ToList();
+        }
+
+        public List<T> ObterRetornoSp<T>(string instSQL)
+        {
+            DbCommand command = _fabricaAcesso.CreateCommand();
+            command.Connection = _con;
+            command.CommandTimeout = int.MaxValue;
+
+            return command.Connection.Query<T>(instSQL, commandType: CommandType.StoredProcedure, transaction: _trans).ToList();
+        }
+
+        public List<T> ObterRetornoSp<T>(string instSQL, params OracleDynamicParameters.ParamInfo[] parametros)
+        {
+            DbCommand command = _fabricaAcesso.CreateCommand();
+            command.Connection = _con;
+            command.CommandTimeout = int.MaxValue;
+
+            var param = OracleParametrosDinamicos(parametros);
+
+            return command.Connection.Query<T>(instSQL, param, commandType: CommandType.StoredProcedure, transaction: _trans).ToList();
+        }
+
+        private static OracleDynamicParameters OracleParametrosDinamicos(OracleDynamicParameters.ParamInfo[] parametros)
+        {
+            OracleDynamicParameters param = new OracleDynamicParameters();
+
+            foreach (var item in parametros)
+            {
+                param.Add(item.Name, dbType: item.DbType, value: item.Value, direction: item.ParameterDirection);
+            }
+
+            return param;
+        }
+
+        public List<T> ObterRetornoSp<T>(string instSQL, DynamicParameters parametros)
+        {
+            DbCommand command = _fabricaAcesso.CreateCommand();
+            command.Connection = _con;
+            command.CommandTimeout = int.MaxValue;
+
+            return command.Connection.Query<T>(instSQL, parametros, commandType: CommandType.StoredProcedure, transaction: _trans).ToList();
+        }
+        #endregion
+
+        #region Sem Dapper
+
+        public DbDataReader retornaSPDR(string instSQL, params DbParameter[] parametros)
+        {
+            DbCommand cmd;
+            cmd = _fabricaAcesso.CreateCommand();
+            cmd.Connection = _con;
+            cmd.CommandText = instSQL;
+            cmd.CommandTimeout = int.MaxValue;
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            if ((parametros != null) && (parametros.Length > 0))
+                cmd.Parameters.AddRange(parametros);
+            if (_comTrasacao)
+                cmd.Transaction = _trans;
+            return cmd.ExecuteReader();
+        }
+
+        #endregion
+    }
+}
